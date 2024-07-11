612c8040d3f0b5ac2c6cf00630e7b6cf9afb5382
Zachary Nicolaou <zack.nicolaou@gmail.com>
2024-07-10 18:11:14 -0700

    updated dmd

--- dmd.py
+++ dmd.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import sys
 import os
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,9 +22,12 @@ def PCA(X,filebase,verbose=False):
             rank=np.where(s<s.max() * max(X.shape[0],X.shape[1]) * np.finfo(X.dtype).eps)[0][0]
         except:
             rank=min(X.shape[0],X.shape[1])
-
+        print('full rank:', rank)
         errs=[]
-        ranks=np.arange(rank//10,rank,rank//10)
+        if rank>10:
+            ranks=np.arange(rank//10,rank,rank//10)
+        else:
+            ranks=np.arange(1,rank)
         for r in ranks:
             errs=errs+[np.linalg.norm(X-u[:,:r].dot(s[:r,np.newaxis]*v[:r]))/np.linalg.norm(X)]
         s[:rank].tofile(filebase+'s.dat')
@@ -70,31 +74,69 @@ def resDMD(U,V,S,X,Y,filebase,verbose=False):
 ###########################################################################
 
 ###########################################################################
-def resDMDpseudo(U,V,S,X,Y,zs,filebase,verbose):
-    if os.path.exists(filebase+'zs.dat') and np.all(np.fromfile(filebase+'zs.dat',dtype=np.complex128)==zs):
-        vals=np.fromfile(filebase+'pseudo.dat')
-    else:
-        start=timeit.default_timer()
+def resDMDpseudo(U,V,S,X,Y,zs,evals,evecs,filebase,verbose):
+    n0=0
+    zs_prev=[]
+    zs_new=zs
+    vals=[]
+    its=[]
+    xis=[]
+    start=timeit.default_timer()
+
+    if os.path.exists(filebase+'zs.dat'):
+        zs_prev=np.fromfile(filebase+'zs.dat',dtype=np.complex128).tolist()
+        zs_new=np.setdiff1d(zs,zs_prev)
+        vals=np.fromfile(filebase+'pseudo.dat',dtype=np.float64).tolist()
+        its=np.fromfile(filebase+'its.dat',dtype=np.int64).tolist()
+        xis=list(np.fromfile(filebase+'xis.dat',dtype=np.complex128).reshape((len(zs_prev),-1)))
+
+    if len(zs_new)>0:
         A=Y.dot(np.conjugate(V).T*1/S)
         B=X.dot(np.conjugate(V).T*1/S)
-        L=np.conjugate(A).T.dot(A)
-        G=np.conjugate(A).T.dot(B)
-        E=np.conjugate(B).T.dot(B)
-        vals=[]
-        for i in range(len(zs)):
-            z=zs[i]
-            if verbose:
-                print('%f'%(i/len(zs)),end='\n',flush=True)
-            C=L-G*z-np.conjugate(G).T*np.conjugate(z)+np.abs(z)**2*E
-            u,s,vh=svd(C)
-            vals=vals+[np.linalg.norm((A-z*B).dot(np.conjugate(vh[-1])))]
-        stop=timeit.default_timer()
-        if verbose:
-            print('pseudospectra runtime:',stop-start,flush=True)
-        np.array(vals).tofile(filebase+'pseudo.dat')
-        np.array(zs).tofile(filebase+'zs.dat')
+        #inverse power iteration with C2
+        for n in range(len(zs_new)):
+            z=zs_new[n]
+            print('%f\t%f\t%f'%(n/len(zs_new),np.real(z),np.imag(z)),end='\n',flush=True)
+            i=np.argmin(np.abs(z-evals))
+            xi=evecs[:,i]
+            A2=(A-z*B)
+            C2=np.conjugate(A2).T.dot(A2)
+            residue=np.linalg.norm(A2.dot(xi))
+            for m in range(100):
+                xi=np.linalg.solve(C2,xi)
+                xi=xi/np.linalg.norm(xi)
+                newres=np.linalg.norm(A2.dot(xi))
+                if np.linalg.norm((residue-newres)/residue)<1E-3:
+                    residue=newres
+                    break
+                residue=newres
+            zs_prev=zs_prev+[z]
+            vals=vals+[residue]
+            xis=xis+[xi]
+            its=its+[m]
+            np.array(zs_prev).tofile(filebase+'zs.dat')
+            np.array(vals).tofile(filebase+'pseudo.dat')
+            np.array(xis).tofile(filebase+'xis.dat')
+            np.array(its).tofile(filebase+'its.dat')
+        #singular value solution with C
+        # L=np.conjugate(A).T.dot(A)
+        # G=np.conjugate(A).T.dot(B)
+        # E=np.conjugate(B).T.dot(B)
+        # vals=[]
+        # for i in range(len(zs)):
+        #     z=zs[i]
+        #     if verbose:
+        #         print('%f'%(i/len(zs)),end='\r',flush=True)
+        #     C=L-G*z-np.conjugate(G).T*np.conjugate(z)+np.abs(z)**2*E
+        #     u,s,vh=svd(C)
+        #     vals=vals+[np.linalg.norm((A-z*B).dot(np.conjugate(vh[-1])))]
+    stop=timeit.default_timer()
+    if verbose:
+        print()
+        print('pseudospectra runtime:',stop-start,flush=True)
 
-    return vals
+
+    return zs_prev,vals,xis,its
 ###########################################################################
 
 if __name__ == "__main__":
@@ -111,7 +153,7 @@ if __name__ == "__main__":
     parser.add_argument("--ni", type=int, required=False, dest='ni', default=26, help='Maximum residue.')
     args = parser.parse_args()
 
-
+    print(*sys.argv,flush=True)
     filebase0 = args.filebase
     verbose = args.verbose
     pcatol = args.pcatol
@@ -144,12 +186,12 @@ if __name__ == "__main__":
         n=n+1
 
     X=np.concatenate([theta[:-1] for theta in thetas],axis=0)
-    X=np.concatenate([np.sin(X),np.cos(X),np.ones((X.shape[0],1)),np.sin(np.sum(X,axis=1)).reshape(-1,1),np.cos(np.sum(X,axis=1)).reshape(-1,1)],axis=1)
+    X=np.concatenate([np.sin(X), np.cos(X), np.ones((X.shape[0],1))],axis=1)
     Y=np.concatenate([theta[1:] for theta in thetas],axis=0)
-    Y=np.concatenate([np.sin(Y),np.cos(Y),np.ones((Y.shape[0],1)),np.sin(np.sum(Y,axis=1)).reshape(-1,1),np.cos(np.sum(Y,axis=1)).reshape(-1,1)],axis=1)
+    Y=np.concatenate([np.sin(Y), np.cos(Y), np.ones((Y.shape[0],1))],axis=1)
 
     if verbose:
-        print('shape:', X.shape)
+        print('shape:', X.shape, flush=True)
 
     s,u,v,errs=PCA(X,filebase0,verbose)
     f=interp1d(errs[0],errs[1])
@@ -158,12 +200,15 @@ if __name__ == "__main__":
         print('rank:',r,flush=True)
     evals,evecs,res=resDMD(u[:,:r],v[:r,:],s[:r],X,Y,filebase0,verbose)
 
-    murs=-scaler+2*scaler*np.arange(nr)/(nr-1)
+    if nr>1:
+        murs=-scaler+2*scaler*np.arange(nr)/(nr-1)
+    else:
+        murs=np.array([0])
     muis=-scalei+2*scalei*np.arange(ni)/(ni-1)
 
     inds=np.intersect1d(np.where(np.abs(np.real(np.log(evals)/dt))<scaler)[0],np.where(np.abs(np.imag(np.log(evals)/dt))<scalei)[0])
     zs=np.concatenate([evals[inds],np.exp((murs[:,np.newaxis]+1j*muis[np.newaxis,:]).ravel()*dt)])
 
-    vals=resDMDpseudo(u[:,:r],v[:r,:],s[:r],X,Y,zs,filebase0,verbose)
+    zs_prevs,vals,xis,its=resDMDpseudo(u[:,:r],v[:r,:],s[:r],X,Y,zs,evals,evecs,filebase0,verbose)
     stop=timeit.default_timer()
     print('runtime:',stop-start)
